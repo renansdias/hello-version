90d9521fe028af9145060f7b29143da089082f96
Renan Dias <renan@flypay.co.uk>
2016-09-07 11:22:30 +0100

    Changed version to 1.0.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var version = "1.0.0";
+var version = "1.0.1";
 
 app.get('/', function(req, res) {
 	res.send('Hello Version: ' + version);
