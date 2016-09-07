85f2a90cd036ae1b770a537131641ee174d1ce57
Renan Dias <renan@flypay.co.uk>
2016-09-07 10:40:32 +0100

    First commit on master

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var version = "1.0.0";
+
+app.get('/', function(req, res) {
+	res.send('Hello Version: ' + version);
+});
+
+app.listen(7890, function() {
+	console.log("Server listening on port 7890");
+});
